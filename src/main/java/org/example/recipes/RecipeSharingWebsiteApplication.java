a47785e515696f7762d3edb3cf5de24ae13c3573
lehuyanh423015 <23021464@vnu.edu.vn>
2025-05-03 16:03:47 +0700

    test

---
+++ src/main/java/org/example/recipes/RecipeSharingWebsiteApplication.java
@@ -0,0 +1,13 @@
+package org.example.recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipeSharingWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RecipeSharingWebsiteApplication.class, args);
+    }
+
+}
